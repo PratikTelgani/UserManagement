2eca50a47bef56503972c5d1308737f10a587184
Pratik Telgani <ptelgani@gmail.com>
2022-06-08 19:46:18 +0530

    App.js file updated

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const mongodb = require('mongodb');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+// mongoose.connect('mongodb+srv://pratshash:pratshash@cluster0.jjquq.mongodb.net/?retryWrites=true&w=majority')
+
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: false}));
+
+//BodyParser
+app.use(express.urlencoded({extended: false}));
+
+//Express session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+})
+); 
+app.use(flash())
+app.use((req, res, next) =>{
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  next();
+})
+
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+
+app.listen(3000, console.log("Server started on port 3000..."));
